087eb4ed6e5f54769d5f64e738c69ad4b7a023bd
Adityaranjan129 <142885100+Adityaranjan129@users.noreply.github.com>
2023-08-22 14:56:43 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,7 +11,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '22502685'))
     API_HASH = str(getenv('API_HASH', 'f1623fc3fd6e62a4ce1a1643dbcd1e39'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','6242881296:AAH2B40XsQrilMvOEhxWzqOxvQ0tlVbvOIo'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','5949426247:AAE2WA0ZnAi6s8yFXPvfFHzrbDCctrlPiEM'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
